3acfae8ea2015789f0a547b057e6ae2b7c01a99b
antrad1978 <antonio@nextgenerationteam.com>
2018-11-10 10:16:39 +0200

    Update s3.py

--- PySpark/s3.py
+++ PySpark/s3.py
@@ -1,16 +1,7 @@
-#https://s3.amazonaws.com/ngtemr/SalesJan2009.csv
-#secret:zVUCH+nUNG4ztJcfq9sijstNgWyxfLpzW5rWLnvh
-#key:AKIAIQG34AFQ7PN6P2SQ
-
-
 import pyspark
 conf = pyspark.SparkConf()
 
 sc = pyspark.SparkContext('local[4]', conf=conf)
-
-
-#sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", "AKIAIQG34AFQ7PN6P2SQ")
-#sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", "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")
 
 bucket = "ngtemr"
 prefix = "SalesJan2009.csv"
